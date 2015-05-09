623316f8ea49b5fc9bba8a7ad2a5d220411c85a8
pongib <pongib@users.noreply.github.com>
2015-05-10 01:12:13 +0700

    Created by Coding Ground Client

---
+++ New Project/36.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ex36()
+{
+    int num, i, j, temp;
+    printf("input number here: ");
+    scanf("%d", &num);
+    temp = num;
+    for(i = 0 ;i < num; i++){
+        
+  	    for(j = temp - 1; j < num; j++){
+  	      printf("x");
+  	    }
+  	    temp--;
+  	    printf("\n");
+    }
+  		    
+    printf("num is %d\n", num);
+    return 0;
+}
+
+
